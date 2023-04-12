ef74f9b35e87d4e80ce910edf9b8d34d03244084
Hussein EL-Husseini <husseine2985@gmail.com>
2023-04-12 17:44:00 +1000

    simple movement controls
    simple movement controls but will be replaced

--- main.js
+++ main.js
@@ -9,13 +9,22 @@ import { OrbitControls } from "./build/three/examples/jsm/controls/OrbitControls
 //import {GLTFLoader} from './build/three/examples/jsm/loaders/GLTFLoader.js';
 import {FirstPersonControls} from 'fps';
 
+const KEYS = {
+  'a': 65,
+  's': 83,
+  'w': 87,
+  'd': 68,
+  'space': 32,
+};
+
 function clamp(x, a, b) {
   return Math.min(Math.max(x, a), b);
 }
 
 //captures key and mouse input
 class InputController {
-  constructor() {
+  constructor(target) {
+    this.target = target || document;
     this.Initialize()
   }
   Initialize() {
@@ -34,14 +43,15 @@ class InputController {
     this.previousKeys = {};
 
     //event listenrs for mouse and keyboard
-    document.addEventListener('mousedown', (e) => this.onMouseDown(e), false);
-    document.addEventListener('mouseup', (e) => this.onMouseUp(e), false);
-    document.addEventListener('mousemove', (e) => this.onMouseMove(e), false);
-    document.addEventListener('keydown', (e) => this.onKeyDown(e), false);
-    document.addEventListener('keyup', (e) => this.onKeyUp(e), false);
+    this.target.addEventListener('mousedown', (e) => this.onMouseDown(e), false);
+    this.target.addEventListener('mouseup', (e) => this.onMouseUp(e), false);
+    this.target.addEventListener('mousemove', (e) => this.onMouseMove(e), false);
+    this.target.addEventListener('keydown', (e) => this.onKeyDown(e), false);
+    this.target.addEventListener('keyup', (e) => this.onKeyUp(e), false);
   }
 //we use these to stash input into the class and use them later
   onMouseDown(e) {
+    this.onMouseMove(e);
     switch(e.button) {
       case 0: {
         this.current.leftButton = true;
@@ -54,6 +64,7 @@ class InputController {
     }
   }
   onMouseUp(e) {
+    this.onMouseMove(e);
     switch(e.button) {
       case 0: {
         this.current.leftButton = false;
@@ -87,12 +98,18 @@ class InputController {
   onKeyUp(e) {
     this.keys[e.keyCode] = false;
   }
+  key(keyCode) {
+    return !!this.keys[keyCode];
+  }
 
   update() {
-    //store and track inputs here
-    this.previous = {
-      ...this.current
-    };
+    //store and track inputs here. this prevents rotyation sliding
+    if (this.previous !== null) {
+      this.current.mouseXDelta = this.current.mouseX - this.previous.mouseX;
+      this.current.mouseYDelta = this.current.mouseY - this.previous.mouseY;
+
+      this.previous = {...this.current};
+    }
   }
 
 }
@@ -102,31 +119,49 @@ class FirstPersonCamera {
     this.camera = camera;
     this.input = new InputController();
     this.rotation = new THREE.Quaternion();
-    this.translation = new THREE.Vector3();
+    //default camera position
+    this.translation = new THREE.Vector3(0, 2, 0);
     this.phi = 0;
     this.theta = 0;
+    this.phiSpeed = 8;
+    this.thetaSpeed = 5;
+   // this.objects_ = objects;
   }
   
   //look in the direction we want
   Update(timeElapsedS) {
     this.UpdateRotation(timeElapsedS);
     this.UpdateCamera(timeElapsedS);
-   
+    this.UpdateTranslation(timeElapsedS);
+    //if input update isnt called, then the rotation will spaz out
+    this.input.update(timeElapsedS);
+    
   }
   
   UpdateCamera(_) {
     this.camera.quaternion.copy(this.rotation);
+   // console.log(this.translation);
+   this.camera.position.copy(this.translation);
+  // this.camera.position.y = 2;
+   //this.camera.y = 10;
+
+ 
+    document.onkeydown = function(e) {
+      console.log(e);
+    }
+  
   }
 
   UpdateRotation() {
+    
     //how far has the mouse moved from preivious frames
     const xh = this.input.current.mouseXDelta / window.innerWidth;
     const yh = this.input.current.mouseYDelta / window.innerHeight;
 
     //since we know the mouse movement, we can now convert x and y movements into spherical coordinates
-    this.phi += -xh * 5;
-    this.theta = Math.min(Math.max(this.theta + -yh * 5, -Math.PI/3), Math.PI/3);
-    //clamp(this.theta + -yh * 5, -Math.PI/3, Math.PI/3);
+    this.phi += -xh * this.thetaSpeed;
+    //this.theta = Math.min(Math.max(this.theta + -yh * 5, -Math.PI/3), Math.PI/3);
+    this.theta= clamp(this.theta + -yh * this.thetaSpeed, -Math.PI/3, Math.PI/3);
 
     //now convert back to rotation
     //rotation about y and x axis
@@ -145,15 +180,38 @@ class FirstPersonCamera {
     this.rotation.copy(q);
     
   }
+
+
+  UpdateTranslation(timeElapsedS) {
+    //this is where movement is handled
+    const forwardVelocity = (this.input.key(KEYS.w) ? 1 : 0) + (this.input.key(KEYS.s) ? -1 : 0);
+    const strafeVelocity = (this.input.key(KEYS.a) ? 1 : 0) + (this.input.key(KEYS.d) ? -1 : 0);
+    //const jumpVelocity = (this.input.key(KEYS.space) ? 1 : 0);
+    const qx = new THREE.Quaternion();
+    qx.setFromAxisAngle(new THREE.Vector3(0, 1, 0), this.phi);
+    //values for moving forward and back
+    const forward = new THREE.Vector3(0, 0, -1);
+    forward.applyQuaternion(qx);
+    forward.multiplyScalar(forwardVelocity * timeElapsedS * 10);
+    //values for moving left and right
+    const left = new THREE.Vector3(-1, 0, 0);
+    left.applyQuaternion(qx);
+    left.multiplyScalar(strafeVelocity * timeElapsedS * 10);
+    
+    this.translation.add(forward);
+    this.translation.add(left);
+  
+  }
+
 }
 
 
 class loadedWorld {
   constructor() {
       this.Initialize(); 
-
+   
       this.InitializeLights();
-      this.InitializeGeos();
+      this.initializeScene_();
       this.InitializeCamera();
    
   }
@@ -176,6 +234,7 @@ class loadedWorld {
       // SCENE
       this.scene = new THREE.Scene();
       this.scene.background = new THREE.Color(0xa8def0);
+      
       //[previous state]
       this.previousRAF = null;
       //animation state
@@ -185,7 +244,11 @@ class loadedWorld {
 
 
       //camera
-      this.camera = new THREE.PerspectiveCamera(160, window.innerWidth / window.innerHeight, 0.1, 1000);
+      const fov = 100;
+      const aspect = 1920 / 1080;
+      const near = 1.0;
+      const far = 1000.0;
+      this.camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
       this.camera.position.set(0, 2, 0);
 
       // const orbitControls = new OrbitControls(this.camera, this.renderer.domElement);
@@ -200,6 +263,147 @@ class loadedWorld {
       
  
   }
+
+  ////////////////////////////////////////////////////////////////////
+  //////////// change any environment here
+  ////////////////////////////////////////////////////////////////////
+  initializeScene_() {
+    //this is a place holder. edit all the scene elements here. then remove the placeholder assets
+    const loader = new THREE.CubeTextureLoader();
+    const texture = loader.load([
+      './resources/skybox/posx.jpg',
+      './resources/skybox/negx.jpg',
+      './resources/skybox/posy.jpg',
+      './resources/skybox/negy.jpg',
+      './resources/skybox/posz.jpg',
+      './resources/skybox/negz.jpg',
+  ]);
+
+    texture.encoding = THREE.sRGBEncoding;
+    this.scene.background = texture;
+
+    const mapLoader = new THREE.TextureLoader();
+    const maxAnisotropy = this.renderer.capabilities.getMaxAnisotropy();
+    const checkerboard = mapLoader.load('resources/checkerboard.png');
+    checkerboard.anisotropy = maxAnisotropy;
+    checkerboard.wrapS = THREE.RepeatWrapping;
+    checkerboard.wrapT = THREE.RepeatWrapping;
+    checkerboard.repeat.set(32, 32);
+    checkerboard.encoding = THREE.sRGBEncoding;
+
+    const plane = new THREE.Mesh(
+        new THREE.PlaneGeometry(100, 100, 10, 10),
+        new THREE.MeshStandardMaterial({map: checkerboard}));
+    plane.castShadow = false;
+    plane.receiveShadow = true;
+    plane.rotation.x = -Math.PI / 2;
+    this.scene.add(plane);
+
+    const box = new THREE.Mesh(
+      new THREE.BoxGeometry(4, 4, 4),
+      this.loadMaterial_('vintage-tile1_', 0.2));
+    box.position.set(10, 2, 0);
+    box.castShadow = true;
+    box.receiveShadow = true;
+    this.scene.add(box);
+
+    const concreteMaterial = this.loadMaterial_('concrete3-', 4);
+
+    const wall1 = new THREE.Mesh(
+      new THREE.BoxGeometry(100, 100, 4),
+      concreteMaterial);
+    wall1.position.set(0, -40, -50);
+    wall1.castShadow = true;
+    wall1.receiveShadow = true;
+    this.scene.add(wall1);
+
+    const wall2 = new THREE.Mesh(
+      new THREE.BoxGeometry(100, 100, 4),
+      concreteMaterial);
+    wall2.position.set(0, -40, 50);
+    wall2.castShadow = true;
+    wall2.receiveShadow = true;
+    this.scene.add(wall2);
+
+    const wall3 = new THREE.Mesh(
+      new THREE.BoxGeometry(4, 100, 100),
+      concreteMaterial);
+    wall3.position.set(50, -40, 0);
+    wall3.castShadow = true;
+    wall3.receiveShadow = true;
+    this.scene.add(wall3);
+
+    const wall4 = new THREE.Mesh(
+      new THREE.BoxGeometry(4, 100, 100),
+      concreteMaterial);
+    wall4.position.set(-50, -40, 0);
+    wall4.castShadow = true;
+    wall4.receiveShadow = true;
+    this.scene.add(wall4);
+
+    // // Create Box3 for each mesh in the scene so that we can
+    // // do some easy intersection tests.
+    const meshes = [
+      plane, box, wall1, wall2, wall3, wall4];
+
+    this.objects_ = [];
+
+    for (let i = 0; i < meshes.length; ++i) {
+      const b = new THREE.Box3();
+      b.setFromObject(meshes[i]);
+      this.objects_.push(b);
+    }
+   
+
+  }
+
+  ////////////////////////////////////////////////////////////////////
+  //////////// change any material loading here
+  ////////////////////////////////////////////////////////////////////
+  loadMaterial_(name, tiling) {
+    //you can use this to load mateirials. edit the stuff in here and remove placeholder assets
+    const mapLoader = new THREE.TextureLoader();
+    const maxAnisotropy = this.renderer.capabilities.getMaxAnisotropy();
+
+    const metalMap = mapLoader.load('resources/freepbr/' + name + 'metallic.png');
+    metalMap.anisotropy = maxAnisotropy;
+    metalMap.wrapS = THREE.RepeatWrapping;
+    metalMap.wrapT = THREE.RepeatWrapping;
+    metalMap.repeat.set(tiling, tiling);
+
+    const albedo = mapLoader.load('resources/freepbr/' + name + 'albedo.png');
+    albedo.anisotropy = maxAnisotropy;
+    albedo.wrapS = THREE.RepeatWrapping;
+    albedo.wrapT = THREE.RepeatWrapping;
+    albedo.repeat.set(tiling, tiling);
+    albedo.encoding = THREE.sRGBEncoding;
+
+    const normalMap = mapLoader.load('resources/freepbr/' + name + 'normal.png');
+    normalMap.anisotropy = maxAnisotropy;
+    normalMap.wrapS = THREE.RepeatWrapping;
+    normalMap.wrapT = THREE.RepeatWrapping;
+    normalMap.repeat.set(tiling, tiling);
+
+    const roughnessMap = mapLoader.load('resources/freepbr/' + name + 'roughness.png');
+    roughnessMap.anisotropy = maxAnisotropy;
+    roughnessMap.wrapS = THREE.RepeatWrapping;
+    roughnessMap.wrapT = THREE.RepeatWrapping;
+    roughnessMap.repeat.set(tiling, tiling);
+
+    const material = new THREE.MeshStandardMaterial({
+      metalnessMap: metalMap,
+      map: albedo,
+      normalMap: normalMap,
+      roughnessMap: roughnessMap,
+    });
+
+    return material;
+  }
+
+
+  ////////////////////////////////////////////////////////////////////
+  //////////// change above the rest below is fine
+  ////////////////////////////////////////////////////////////////////
 
   InitializeCamera() {
     //put Camera elements in here!
